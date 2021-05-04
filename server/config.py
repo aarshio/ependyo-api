07b4c5055ecdb43e8cb909e221095d675dd1c847
Aarsh <aarsh937@gmail.com>
2021-05-04 18:57:45 -0400

    first commit

--- server/config.py
+++ server/config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = '74d05d9f7bbd23126bf5968baaa1eeb3'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'ependyo.help'
+    MAIL_PASSWORD = 'Ependyo@171'
